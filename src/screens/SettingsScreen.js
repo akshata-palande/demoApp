5482c498d46df5c1afd1da96dac8701f0458906e
Akshata Palande <akshata.palande@wipro.com>
2021-03-19 23:21:14 +0530

    navigation added with 2 screens

---
+++ src/screens/SettingsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function SettingsScreen() {
+  return (
+    <View>
+      <Text>Screen 2</Text>
+    </View>
+  );
+}
